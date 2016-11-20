144669b243a1e2f5a17c1972764a845a143dacee
Kristian Mandrup <kmandrup@gmail.com>
2016-11-20 13:26:02 +0100

    ava test

--- src/operator/code.test.js
+++ src/operator/code.test.js
@@ -1,11 +1,16 @@
-import { expect } from 'chai'
+// import { expect } from 'chai'
+import test from 'ava'
 import code from './code'
-import test from 'mocha-test-dsl'
+// import test from 'mocha-test-dsl'
 
-test('Operator: code')
-  .that('an instance')
-  .should('have a name', () => {
-    let result = code('kris')
-    expect(result).to.eql(true)
-  })
-  .run()
+// test('Operator: code')
+//   .that('an instance')
+//   .should('have a name', () => {
+//     let result = code('kris')
+//     expect(result).to.eql(true)
+//   })
+//   .run()
+
+test('hello', t => {
+  t.true(true)
+})
